a0b916dc593f490dbae669817d95c9b47a0bdedf
Vaidh <vedprakahsingh216@gmail.com>
2022-03-28 01:51:00 +0530

    first commit

--- quiz_game.py
+++ quiz_game.py
@@ -1,8 +1,34 @@
-print("Welcome to computer quiz!")
+print("Welcome to gaming quiz!")
 
 playing = input("Dp you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
+
+answer = input("What does PUBG stands for ? ")
+if answer.lower() == "playerUnknown's battlegrounds":
+    print("Correct! ")
 else:
-    print("Playing")
+    print("Incorrect!")
+
+
+answer = input("What does COC stands for ? ")
+if answer.lower() == "clash of clan":
+    print("Correct! ")
+else:
+    print("Incorrect!")
+
+
+answer = input("What does COD Stands for ? ")
+if answer.lower() == "call of duty":
+    print("Correct! ")
+else:
+    print("Incorrect!")
+
+
+answer = input("what does CS-GO stands for ? ")
+if answer.lower() == "counter strike global offensive":
+    print("Correct! ")
+else:
+    print("Incorrect!")
+
